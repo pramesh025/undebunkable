7776adae90947b2ac3b837180dfeba936586e514
Pramesh <076bei025.pramesh@pcampus.edu.np>
2022-08-18 08:10:55 +0545

    update updated

--- update_data.js
+++ update_data.js
@@ -33,5 +33,88 @@ function print_menu(){
 }
 
 function update_student_data(){
-    
+    var id = readline.question("Enter student id(eg 076bei001): ");
+    pool.query("SELECT * FROM student WHERE std_id = '" + id + "'",(err, res) => {
+        if(err) {
+            console.log(err);
+        }
+        else {
+            console.table(res.rows);
+            if(res.rows.length > 0){
+                if(readline.question("Are you sure you want to update this student?(y/n): ").toLowerCase() == "y"){
+                    var name = readline.question("Enter student name: ");
+                    var email = readline.question("Enter student email: ");
+                    var isCR = readline.question("Enter student isCR(t/f): ");
+                    var password = readline.question("Enter student password: ");
+                    pool.query("UPDATE student SET name='" + name + "', email='" + email + "', is_cr='" + isCR + "', password='" + password + "' WHERE std_id='"+id+"'",(err, res) => {
+                        if(err) {
+                            console.log(err);
+                        }
+                        else {
+                            console.log("Student updated successfully");
+                        }
+                    }
+                    );
+                }
+            }
+        }
+    });
 }
+
+function update_teacher_data(){
+    var id = readline.question("Enter teacher id(eg BS): ");
+    pool.query("SELECT * FROM teacher WHERE teacher_id = '" + id + "'",(err, res) => {
+        if(err) {
+            console.log(err);
+        }
+        else {
+            console.table(res.rows);
+            if(res.rows.length > 0){
+                if(readline.question("Are you sure you want to update this teacher?(y/n): ").toLowerCase() == "y"){
+                    var name = readline.question("Enter teacher name: ");
+                    var email = readline.question("Enter teacher email: ");
+                    pool.query("UPDATE teacher SET name='" + name + "', email='" + email + "'WHERE teacher_id='"+id+"'",(err, res) => {
+                        if(err) {
+                            console.log(err);
+                        }
+                        else {
+                            console.log("Teacher updated successfully");
+                        }
+                    }
+                    );
+                }
+            }
+        }
+    });
+}
+
+function update_subject_data(){
+    var id = readline.question("Enter subject id(eg CT610): ");
+    pool.query("SELECT * FROM subject WHERE subject_id = '" + id + "'",(err, res) => {
+        if(err) {
+            console.log(err);
+        }
+        else {
+            console.table(res.rows);
+            if(res.rows.length > 0){
+                if(readline.question("Are you sure you want to update this subject?(y/n): ").toLowerCase() == "y"){
+                    var name = readline.question("Enter subject name: ");
+                    pool.query("UPDATE subject SET name='" + name+"' WHERE subject_id='"+id + "'", (err, res) => {
+                        if(err) {
+                            console.log(err);
+                        }
+                        else {
+                            console.log("Subject updated successfully");
+                        }
+                    }
+                    );
+                }
+            }
+        }
+    }
+    );
+
+
+}
+
+print_menu();
